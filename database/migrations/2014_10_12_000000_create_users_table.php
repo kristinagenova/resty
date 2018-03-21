d5ddd2407cc6e3eb89b614c4020780ce50c4c461
kristina <kristina.r.genova@gmail.com>
2018-03-21 21:51:14 +0000

    test

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('user_id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('api_token', 1000)->unique();
+            $table->string('api_token', 500)->unique();
             $table->string('social_network_token');
             $table->string('social_network');
             $table->string('avatar')->default('');
