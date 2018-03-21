e07c7d53bf573de60e3403945a3c3502edf0b076
kristina <kristina.r.genova@gmail.com>
2018-03-21 13:20:36 +0000

    changed default value for actual_notification_time

--- database/migrations/2018_03_12_105159_add_actual_notification_time_to_meal_settings_table.php
+++ database/migrations/2018_03_12_105159_add_actual_notification_time_to_meal_settings_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddActualNotificationTimeToMealSettingsTable extends Migration
 {
@@ -14,7 +14,7 @@ class AddActualNotificationTimeToMealSettingsTable extends Migration
     public function up()
     {
         Schema::table("meal_settings", function (Blueprint $table){
-            $table->date('actual_notification_time')->default('');
+            $table->timestamp('actual_notification_time')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
 
